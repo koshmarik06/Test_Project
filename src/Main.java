002c412f5fbbe0738a7cc39865d9bfd0a0547873
koshmarik06 <koshmarik06@mail.ru>
2023-04-27 16:10:34 +0700

    Привет Максиму

--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,17 @@ public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello all world!");
-        System.out.println(" ");
+        System.out.println("Здравствуйте мои маленькие любители экстримизма!");
+        System.out.println("Сегодня мы будем умножать числа на десять");
         System.out.println("-----------------");
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        for (int i = 1; i <= 9; i++) {
 
             // Press Shift+F9 to start debugging your code. We have set one breakpoint
             // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+            System.out.println("i = " + i + " а вот i*10 = " + i*10);
         }
         System.out.println("-----------------");
+        System.out.println("Я уверен тебе очень важна эта информация! Пока.");
     }
 }
